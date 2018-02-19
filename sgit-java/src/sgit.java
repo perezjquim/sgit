974a755d594f3f01003dbedcb13499b2d2f6ae01
Joaquim Perez <jquim1@hotmail.com>
2018-02-19 19:34:06 +0000

    Analysis and History panels done

--- sgit-java/src/sgit.java
+++ sgit-java/src/sgit.java
@@ -1,11 +1,99 @@
 package sgit;
+import perezjquim.GUI.*;
+import perezjquim.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
 
 public class sgit
 {
-
+	private static GUI main;
+	private static ScrollableTextArea console;
 	public static void main(String[] args)
 	{
-		System.out.println("Great success");
-	}
+		main = new GUI("Sgit");
 
+		Panel panAnalysis = new Panel("Analysis");
+		panAnalysis.add(new Button("Status",()->
+			{
+				status();
+			}));
+		panAnalysis.add(new Button("Diff",()->
+			{
+				diff();
+			}));
+		main.add(panAnalysis);
+
+		Panel panHistory = new Panel("History");
+		panHistory.add(new Button("Timeline",()->
+			{
+				timeline();
+			}));
+		panHistory.add(new Button("Throwback",()->
+			{
+				throwback();
+			}));
+		panHistory.add(new Button("Current",()->
+			{
+				current();
+			}));										
+		main.add(panHistory);
+
+		Panel panConsole = new Panel();
+		console = new ScrollableTextArea(TextArea.HEIGHT_MEDIUM,TextArea.WIDTH_LARGE);
+		panConsole.add(console);
+		main.add(panConsole);
+
+		main.start();
+	}
+	private static void status()
+	{
+		console.getTextArea().clear();
+		try
+		{
+			Cmd.exec("sgit status",console.getTextArea());
+		}
+		catch(IOException e)
+		{ e.printStackTrace(); }
+	}
+	private static void diff()
+	{
+		console.getTextArea().clear();		
+		try
+		{				
+			Cmd.exec("sgit diff",console.getTextArea());
+		}
+		catch(IOException e)
+		{ e.printStackTrace(); }
+	}
+	private static void timeline()
+	{
+		console.getTextArea().clear();		
+		try
+		{				
+			Cmd.exec("sgit timeline",console.getTextArea());
+		}
+		catch(IOException e)
+		{ e.printStackTrace(); }		
+	}
+	private static void throwback()
+	{
+		console.getTextArea().clear();		
+		try
+		{				
+			Cmd.exec("sgit timeline",console.getTextArea());
+		}
+		catch(IOException e)
+		{ e.printStackTrace(); }			
+	}
+	private static void current()
+	{
+		console.getTextArea().clear();		
+		try
+		{				
+			Cmd.exec("sgit current",console.getTextArea());
+		}
+		catch(IOException e)
+		{ e.printStackTrace(); }			
+	}
 }
